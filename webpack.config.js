963ff1883bcbc3a37b29b52b07f98de8f2d9c893
cnAlan <2522658159@qq.com>
2019-03-16 21:26:41 +0800

    update some file

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader'
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './public'),
+        historyApiFallback: true,
+        compress: true,
+        port: 3000,
+        open: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './public/index.html')
+        })
+    ]
+
+
+}
